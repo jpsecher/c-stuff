771c93d39939d0f1392884ca4734eb6bf10da64c
Jens Peter Secher <jpsecher@gmail.com>
2021-03-25 09:34:50 +0100

    netmask_from_str

--- network/netmask-test.cpp
+++ network/netmask-test.cpp
@@ -19,9 +19,24 @@ TEST(Netmask, Encode)
     UNSIGNED_LONGS_EQUAL(16, netmask_encode(0xFFFF0000));
 }
 
-TEST(Netmask, String)
+TEST(Netmask, ToString)
 {
     STRCMP_EQUAL("255.255.255.0", netmask_to_str(0xFFFFFF00));
     STRCMP_EQUAL("255.255.248.0", netmask_to_str(0xFFFFF800));
     STRCMP_EQUAL("255.0.0.0", netmask_to_str(0xFF000000));
+}
+
+TEST(Netmask, FromString)
+{
+    UNSIGNED_LONGS_EQUAL(0xFFFFFF00, netmask_from_str("255.255.255.0"));
+    UNSIGNED_LONGS_EQUAL(0xFFFFFFFF, netmask_from_str("255.255.255.255"));
+    UNSIGNED_LONGS_EQUAL(0xFFFFF800, netmask_from_str("255.255.248.0"));
+    UNSIGNED_LONGS_EQUAL(0x0A080800, netmask_from_str("10.8.8.0"));
+}
+
+TEST(Netmask, Octet)
+{
+    UNSIGNED_LONGS_EQUAL(1, octet_str_len(0));
+    UNSIGNED_LONGS_EQUAL(2, octet_str_len(10));
+    UNSIGNED_LONGS_EQUAL(3, octet_str_len(128));
 }
--- network/netmask.cpp
+++ network/netmask.cpp
@@ -1,5 +1,6 @@
 #include "netmask.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 uint32_t netmask_decode(uint8_t bits)
 {
@@ -18,4 +19,26 @@ char const * netmask_to_str(uint32_t mask)
     static char buffer[16];
     snprintf(buffer, 16, "%d.%d.%d.%d", (mask >> 24), (mask >> 16) & 0xFF, (mask >> 8) & 0xFF, mask & 0xFF);
     return buffer;
+}
+
+uint8_t octet_str_len(uint8_t number)
+{
+    if (number < 10)
+        return 1;
+    if (number < 100)
+        return 2;
+    return 3;
+}
+
+uint32_t netmask_from_str(char const * str)
+{
+    uint8_t index = 0;
+    int i1 = atoi(str + index);
+    index += octet_str_len(i1) + 1;
+    int i2 = atoi(str + index);
+    index += octet_str_len(i2) + 1;
+    int i3 = atoi(str + index);
+    index += octet_str_len(i3) + 1;
+    int i4 = atoi(str + index);
+    return (i1 << 24) | (i2 << 16) | (i3 << 8) | i4;
 }
